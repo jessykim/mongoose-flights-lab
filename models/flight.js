40c23e472b9eb442dfa5b34ae2ec35f819ec1d10
jessikim <jessica.kim17@gmail.com>
2022-09-23 18:20:47 -0500

    set flight schema in models and export

---
+++ models/flight.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose"
+
+const Schema = mongoose.Schema
+
+const flightSchema = new Schema({
+  airline: { 
+    type: String,
+    enum: ['American', 'Southwest', 'United']
+  },
+  airport: {
+    type: String,
+    enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+    default: 'DEN'
+  },
+  flightNo: { 
+    type: Number,
+    min: 0,
+    max: 9999,
+  },
+  departs: { 
+    type: Date, 
+    default: () => new Date(+new Date() + 365*24*60*60*1000)
+  }, 
+}, {
+  timestamps: true
+})
+
+const Flight = mongoose.model('Flight', flightSchema)
+
+export {
+  Flight
+}
