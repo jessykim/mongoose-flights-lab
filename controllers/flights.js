004d46d04bb06bab1a802f68b5cc186c3523a5d2
jessikim <jessica.kim17@gmail.com>
2022-09-26 15:49:36 -0500

    complete createTicket function and console.log to test

--- controllers/flights.js
+++ controllers/flights.js
@@ -86,7 +86,23 @@ function update(req, res) {
 }
 
 function createTicket(req, res) {
-  console.log('new tickets!');
+  console.log('req.body', req.body);
+  Flight.findById(req.params.id)
+  .then(flight => {
+    flight.tickets.push(req.body)
+    flight.save()
+    .then(() => {
+      res.redirect(`/flights/${flight._id}`)
+    })
+    .catch(err => {
+      console.log(err);
+      res.redirect('/')
+    })
+  })
+  .catch(err => {
+    console.log(err);
+    res.redirect('/')
+  })
 }
 
 export {
